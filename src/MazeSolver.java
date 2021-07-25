c09c2d92fec74f16962b5b357911d353ded62ae0
MohJumper <qureshimemo@gmail.com>
2021-07-24 21:34:16 -0400

    need to work on edge cases

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -3,9 +3,10 @@ import java.util.LinkedList;
 public class MazeSolver {
     // static can be simple instantiated
     static int[][] maze = {
-            {2, 0, 1, 1},
-            {1, 1, 0, 1},
-            {0, 1, 1, 1}
+            {1, 0, 1, 1},
+            {1, 1, 1, 0},
+            {0, 1, 1, 2},
+            {0, 1, 0, 0}
     };
     // 0 = wall
     // 1 = path
@@ -18,6 +19,52 @@ public class MazeSolver {
         // push to the start so we can create a stack
         path.push(sp);
         // [row][column]
-        maze[path.peek().x][path.peek().y] = 0;
+
+
+
+        while(true){
+            int x = path.peek().x;
+            int y = path.peek().y;
+            maze[y][x] = 0;
+            // moving down
+            if(maze[y+1][x] == 2){
+                System.out.println("Moved down. you Won!");
+                return;
+            } else if(maze[y+1][x] == 1){
+                System.out.println("Move down!");
+                path.push(new StartingPosition(y+1, x));
+                continue;
+            }
+            // move left
+            if(maze[y][x-1] == 2){
+                System.out.println("Moved left. you Won!");
+                return;
+            } else if(maze[y][x-1] == 1){
+                System.out.println("Move left!");
+                path.push(new StartingPosition(y, x-1));
+                continue;
+            }
+            // move up
+            if(maze[y-1][x] == 2){
+                System.out.println("Moved up. you Won!");
+                return;
+            } else if(maze[y-1][x] == 1){
+                System.out.println("Move up!");
+                path.push(new StartingPosition(y-1, x));
+                continue;
+            }
+            // move right
+            if(maze[y][x+1] == 2){
+                System.out.println("Moved right. you Won!");
+                return;
+            } else if(maze[y][x+1] == 1){
+                System.out.println("Move right!");
+                path.push(new StartingPosition(y, x+1));
+                continue;
+            }
+
+
+
+        }
     }
 }
